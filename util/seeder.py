1d746df78f6cb2e684cc79142556822fee084f01
Luca Di Bello <lucadibello@proton.me>
2025-04-09 15:06:33 +0200

    Created utility files

---
+++ util/seeder.py
@@ -0,0 +1,22 @@
+import os
+import random
+import numpy as np
+import torch
+
+def seed_everything(seed: int):
+    # Python random module
+    random.seed(seed)
+    # Numpy random module
+    np.random.seed(seed)
+    # Torch random seeds
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+
+    # Set PYTHONHASHSEED environment variable for hash-based operations
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+    # Ensure deterministic behavior in cudnn (may slow down your training)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
