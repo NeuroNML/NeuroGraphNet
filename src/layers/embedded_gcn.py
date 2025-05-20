fde0dbfcfb3074dbdc713d3ce715efe449e7bae2
gbelis <81580055+gbelis@users.noreply.github.com>
2025-05-20 14:01:39 +0200

    Update embedded_gcn.py

--- src/layers/embedded_gcn.py
+++ src/layers/embedded_gcn.py
@@ -1,7 +1,7 @@
 import torch
-import torch.nn as nn
+import torch.nn as nn, BatchNorm1d
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, BatchNorm1d, global_mean_pool
+from torch_geometric.nn import GCNConv, global_mean_pool
 from torch_geometric.nn import Linear
 from layers.eeggcn import EEGGCN
 
